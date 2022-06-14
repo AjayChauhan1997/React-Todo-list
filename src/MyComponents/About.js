fca010c5c1548589678d08a91eedb81a4497ff2a
AjayChauhan1997 <ajay1997.official@gmail.com>
2022-06-14 21:30:53 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div className='babout'>
+            <h3>About project</h3> 
+            <p>This project is used to notedown the list of work which a individual wants to do so that 
+                when individual see the applicatin he/she will get the list of todo items. </p>
+
+                <h4>How the applicatin work</h4>
+                <p>This appliaction take a todo tittle and todo descreption as input and add it to your 
+                    todolst. 
+                    <p className='pabout'>Note tittle and descreption never bi blank.</p>
+                 
+                </p>
+        </div>
+    )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const Footer = () => { 
+    return (
+        <footer >
+            <p >
+            Copyright &copy; MyTodosList.com
+            </p>
+        </footer>
+    )
+}
